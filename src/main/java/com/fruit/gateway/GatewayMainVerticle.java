8b46f9fa5fe22a7457e727c118258e4cc277d998
vincent78 <vincent78@163.com>
2018-09-25 16:12:13 +0800

    代码结构的调整

--- src/main/java/com/fruit/gateway/GatewayMainVerticle.java
+++ src/main/java/com/fruit/gateway/GatewayMainVerticle.java
@@ -3,15 +3,25 @@ package com.fruit.gateway;
 import com.fruit.shovel.SVConstant;
 import com.fruit.shovel.ShovelMainVerticle;
 import com.fruit.shovel.annotation.EventMethod;
-import com.fruit.shovel.manager.VerticleManager;
-import com.fruit.shovel.utils.HandlerUtil;
+import com.fruit.shovel.utils.MsgIFQueue;
 import io.vertx.core.eventbus.Message;
 
 public class GatewayMainVerticle extends ShovelMainVerticle {
 
     @EventMethod(SVConstant.EVENT_START_DEFAULT)
     public void startDefault(Message<?> msg) {
-        VerticleManager.send(SVConstant.EVENT_STARTREDAY,null, HandlerUtil.handlerTransfer2Msg(msg));
+
+        new MsgIFQueue().thenCompose(SVConstant.EVENT_MONGODB_INIT)
+                        .thenCompose(SVConstant.EVENT_REDIS_INIT)
+                        .thenCompose(SVConstant.EVENT_KAFKA_CONSUMER_INIT)
+                        .thenCompose(SVConstant.EVENT_STARTREDAY)
+                        .whenComplete((obj, e) -> {
+                            if (e != null) {
+                                msg.fail(100, e.getMessage());
+                            } else {
+                                msg.reply(obj);
+                            }
+                        }).start();
     }
 
 }
