107d76c8ce75a14674740f8418d43dcd85d84ec1
vincent78 <vincent78@163.com>
2018-10-11 20:33:52 +0800

    format code

--- src/main/java/com/fruit/gateway/verticle/ServiceVerticle.java
+++ src/main/java/com/fruit/gateway/verticle/ServiceVerticle.java
@@ -1,11 +1,8 @@
 package com.fruit.gateway.verticle;
 
 import com.fruit.gateway.Constant;
-import com.fruit.shovel.SVConstant;
 import com.fruit.shovel.annotation.EventMethod;
 import com.fruit.shovel.annotation.Verticle;
-import com.fruit.shovel.manager.ClusterConfManager;
-import com.fruit.shovel.manager.WebManager;
 import com.fruit.shovel.verticle.VerticleBase;
 import io.vertx.core.eventbus.Message;
 
@@ -13,19 +10,8 @@ import io.vertx.core.eventbus.Message;
 public class ServiceVerticle extends VerticleBase {
 
     @EventMethod(Constant.EVENT_REGIST_SERVICE)
-    public void registService(Message msg) {
-        WebManager manager = WebManager.getInstance();
-        String root = manager.getVirtualPathPrefix();
-        //时间戳
-        manager.registServiceRoute(SVConstant.EVENT_TIMESTAMP, root + "/timestamp");
-
-        //verticle的信息
-        manager.registServiceRoute(SVConstant.EVENT_BASE_VERTICLE_INSTANCE, root + "/vInfo");
-
-        if (ClusterConfManager.isClustered()) {
-            //查询brokers
-            manager.registServiceRoute(SVConstant.EVENT_CLUSTER_BROKERS_INFO, root + "/brokers");
-        }
+    public void initService(Message msg) {
         msg.reply(true);
     }
+
 }
--- src/main/java/com/fruit/gateway/verticle/TestVerticle.java
+++ src/main/java/com/fruit/gateway/verticle/TestVerticle.java
@@ -5,7 +5,8 @@ import com.fruit.shovel.annotation.Verticle;
 import com.fruit.shovel.verticle.VerticleBase;
 import io.vertx.core.eventbus.Message;
 
-@Verticle
+@Verticle(value = "com.fruit.gateway.verticle", instances = 3)
+@SuppressWarnings("unused")
 public class TestVerticle extends VerticleBase {
 
     @EventMethod("gateway_test")
